15d285abb8785b96c46ecdeb83ca52ccb53362f7
jrivas2 <jarivas2@gmail.com>
2016-10-05 12:43:21 -0500

    Initial commit

---
+++ app/src/main/java/com/jrivas2/mitarea/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jrivas2.mitarea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
